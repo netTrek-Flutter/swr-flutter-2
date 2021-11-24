6686c8b7a7d284bb5733b07d4a0a5c5fe9e15fcb
Saban Ünlü [netTrek] <us@netTrek.de>
2021-11-24 16:36:42 +0100

    Buttons and Styles - part 1

--- lib/samples/button_sample.dart
+++ lib/samples/button_sample.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:training/common/my_flutter_icons_icons.dart';
 
 class ButtonSample extends StatelessWidget {
   const ButtonSample({Key? key}) : super(key: key);
@@ -8,12 +9,98 @@ class ButtonSample extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        TextButtonTheme(
+          // Theming über inherited Widtgets - Theme wird in den Context geschoben
+          data: TextButtonThemeData(
+              style: TextButton.styleFrom(
+            primary: Colors.green,
+          )),
+          child: TextButton(
+            onPressed: () {
+              print('Clicked on TextButton');
+            },
+            child: const Text('TextButton'),
+          ),
+        ),
+        TextButton(
+          onPressed: () {
+            print('Clicked on TextButton');
+          },
+          style: TextButton.styleFrom(
+            primary: Colors.red,
+          ),
+          child: const Text('TextButton'),
+        ),
         TextButton(
           onPressed: () {
             print('Clicked on TextButton');
           },
           child: const Text('TextButton'),
-        )
+        ),
+        const TextButton(
+          onPressed: null, // disable Button
+          child: Text('TextButton'),
+        ),
+        IconButton(
+          onPressed: () {
+            print('Clicked on IconButton');
+          },
+          icon: const Icon(Icons.add),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            print('clicked on ElevatedButton');
+          },
+          style: ElevatedButton.styleFrom(primary: Colors.amber),
+          child: const Text('ElevatedButton'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            print('clicked on ElevatedButton');
+          },
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+            textStyle: MaterialStateProperty.all<TextStyle>(
+                const TextStyle(fontFamily: 'Monospace')),
+          ),
+          child: const Text('ElevatedButton'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            print('clicked on ElevatedButton');
+          },
+          style: ButtonStyle(
+            backgroundColor:
+                MaterialStateProperty.resolveWith<Color?>((states) {
+              if (states.contains(MaterialState.pressed)) {
+                // return Colors.tealAccent;
+                return Theme.of(context).colorScheme.primaryVariant;
+              }
+              return null;
+            }),
+            textStyle: MaterialStateProperty.resolveWith<TextStyle>((states) {
+              if (states.contains(MaterialState.pressed)) {
+                // return Colors.tealAccent;
+                return const TextStyle(decoration: TextDecoration.underline);
+              }
+              return const TextStyle();
+            }),
+          ),
+          child: const Text('ElevatedButton'),
+        ),
+        OutlinedButton(
+          onPressed: () {
+            print('clicked on OutlinedButton');
+          },
+          child: const Text('OutlinedButton'),
+        ),
+        OutlinedButton.icon(
+          onPressed: () {
+            print('clicked on OutlinedButton');
+          },
+          label: const Text('OutlinedButton with Icon'),
+          icon: const Icon(Icons.add),
+        ),
       ],
     );
   }
