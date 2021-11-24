110e861c660a34eabda0fb1bfa537928f0e02b24
Saban Ünlü [netTrek] <us@netTrek.de>
2021-11-24 15:40:14 +0100

    Assets and MyFlutterIcons

--- lib/samples/icons_sample.dart
+++ lib/samples/icons_sample.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:training/common/my_flutter_icons_icons.dart';
 
 class IconSample extends StatelessWidget {
   const IconSample({Key? key}) : super(key: key);
@@ -20,6 +21,11 @@ class IconSample extends StatelessWidget {
         Icon(
           Icons.add,
           size: 48,
+        ), // default
+        Icon(
+          MyFlutterIcons.nettrek_logo,
+          size: 72,
+          color: Colors.red,
         ),
         // Icon(CupertinoIcons.camera_on_rectangle),
         // Icon(Icons.adaptive.flip_camera),
