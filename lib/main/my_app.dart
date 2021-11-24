796cadc282e78b93337230d54cc561f9893959e7
Saban Ünlü [netTrek] <us@netTrek.de>
2021-11-24 10:25:11 +0100

    own themeData

--- lib/main/my_app.dart
+++ lib/main/my_app.dart
@@ -10,6 +10,34 @@ final ThemeData lightVariant = ThemeData.from(
   ),
 );
 
+final ThemeData lightVariantWithTextStyles = ThemeData.from(
+  colorScheme: const ColorScheme.light(
+    primary: Colors.amber,
+    secondary: Colors.orange,
+    onPrimary: Colors.black,
+    // background: Colors.grey,
+  ),
+  textTheme: const TextTheme(
+      bodyText2: TextStyle(
+    color: Colors.green,
+  )),
+);
+
+final ThemeData myTheme = ThemeData(
+  colorScheme: const ColorScheme.light(
+    primary: Colors.amber,
+    secondary: Colors.orange,
+    onPrimary: Colors.black,
+    // background: Colors.grey,
+  ),
+  fontFamily: 'Monospace',
+  textTheme: const TextTheme(
+    bodyText2: TextStyle(
+      color: Colors.green,
+    ),
+  ),
+);
+
 class MyApp extends StatelessWidget {
   final Widget body;
   const MyApp({
@@ -20,7 +48,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: lightVariant,
+      theme: myTheme,
       darkTheme: ThemeData.from(
         colorScheme: const ColorScheme.dark(),
       ),
